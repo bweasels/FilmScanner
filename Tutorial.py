aca4904c03ce84d9979b3bef2bb243c9de8ada1c
Ben Wesley <benkumwes@gmail.com>
2020-07-12 12:14:22 -0400

    Dumb mistake

--- Tutorial.py
+++ Tutorial.py
@@ -5,7 +5,7 @@ import time
 import cv2
 # initialize the camera and grab a reference to the raw camera capture
 camera = PiCamera()
-camera.resolution(800, 600)
+camera.resolution = (800, 600)
 rawCapture = PiRGBArray(camera)
 # allow the camera to warmup
 time.sleep(0.1)
