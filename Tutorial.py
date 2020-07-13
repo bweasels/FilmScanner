b7e287b44dbcd850cfbd0f604bd4b150af164c32
Ben Wesley <benkumwes@gmail.com>
2020-07-12 23:05:42 -0400

    trying a new tutorial

--- Tutorial.py
+++ Tutorial.py
@@ -6,7 +6,7 @@ with picamera.PiCamera() as camera:
     with picamera.array.PiRGBArray(camera) as stream:
         camera.resolution = (100, 100)
         camera.start_preview()
-        time.sleep(2)
+        time.sleep(10)
         camera.capture(stream, 'rgb')
         # Show size of RGB data
         print(stream.array.shape)
