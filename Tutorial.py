365a96a105cbb2689be1c02a589f921ff1c9a7cf
Ben Wesley <benkumwes@gmail.com>
2020-07-12 22:51:23 -0400

    giving up on resizing to see if the error persists

--- Tutorial.py
+++ Tutorial.py
@@ -3,17 +3,33 @@ from picamera.array import PiRGBArray
 from picamera import PiCamera
 import time
 import cv2
+import io
+
 # initialize the camera and grab a reference to the raw camera capture
 camera = PiCamera()
-rawCapture = PiRGBArray(camera)
-# allow the camera to warmup
-time.sleep(0.1)
-i = 0
-while i < 1000:
-    # grab an image from the camera
-    camera.capture(rawCapture, format="bgr")
-    image = rawCapture.array
+stream = io.BytesIO()
 
-    # display the image on screen and wait for a keypress
-    cv2.imshow("Image", image)
-    i = i + 1
+# capture frames from the camera
+for frame in camera.capture_continuous(stream, format="jpg"):
+    # grab the raw NumPy array representing the image, then initialize the timestamp
+    # and occupied/unoccupied text
+    image = frame.array
+    cv2.imshow('frame', image)
+    stream.truncate()
+    stream.seek(0)
+    if process(stream):
+        break
+
+cv2.destroyAllWindows()
+#
+# # allow the camera to warmup
+# time.sleep(0.1)
+# i = 0
+# while i < 1000:
+#     # grab an image from the camera
+#     camera.capture(rawCapture, format="bgr")
+#     image = rawCapture.array
+#
+#     # display the image on screen and wait for a keypress
+#     cv2.imshow("Image", image)
+#     i = i + 1
