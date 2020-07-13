f91dc6f41f0f312bd25f9a6f4a0717f7cac2b073
Ben Wesley <benkumwes@gmail.com>
2020-07-12 23:13:54 -0400

    yet another tutorial - I wanna use cv2 with it

--- Tutorial.py
+++ Tutorial.py
@@ -17,58 +17,10 @@ for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=
 	image = frame.array
 	# show the frame
 	cv2.imshow("Frame", image)
+	cv2.imshow('Inverted', cv2.bitwise_not(image))
 	key = cv2.waitKey(1) & 0xFF
 	# clear the stream in preparation for the next frame
 	rawCapture.truncate(0)
 	# if the `q` key was pressed, break from the loop
 	if key == ord("q"):
 		break
-
-# import time
-# import picamera
-# import picamera.array
-#
-# with picamera.PiCamera() as camera:
-#     with picamera.array.PiRGBArray(camera) as stream:
-#         camera.resolution = (800, 600)
-#         camera.start_preview()
-#         time.sleep(10)
-#         camera.capture(stream, 'rgb')
-#         # Show size of RGB data
-#         print(stream.array.shape)
-
-# # import the necessary packages
-# from picamera.array import PiRGBArray
-# from picamera import PiCamera
-# import time
-# import cv2
-# import io
-#
-# # initialize the camera and grab a reference to the raw camera capture
-# camera = PiCamera()
-# stream = io.BytesIO()
-#
-# # capture frames from the camera
-# for frame in camera.capture_continuous(stream, format="rgb"):
-#     # grab the raw NumPy array representing the image, then initialize the timestamp
-#     # and occupied/unoccupied text
-#     # image = frame.array
-#     cv2.imshow('frame', frame)
-#     stream.truncate()
-#     stream.seek(0)
-#     if process(stream):
-#         break
-#
-# cv2.destroyAllWindows()
-# #
-# # # allow the camera to warmup
-# time.sleep(0.1)
-# i = 0
-# while i < 1000:
-#     # grab an image from the camera
-#     camera.capture(rawCapture, format="bgr")
-#     image = rawCapture.array
-#
-#     # display the image on screen and wait for a keypress
-#     cv2.imshow("Image", image)
-#     i = i + 1
