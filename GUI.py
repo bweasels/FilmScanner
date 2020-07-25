5fed1abcd3d4756d30be56532d1c3d53de1931d1
Ben Wesley <benkumwes@gmail.com>
2020-07-25 12:20:44 -0400

    Finalizing GUI and making page superclass

--- GUI.py
+++ GUI.py
@@ -20,17 +20,33 @@ Config.set('graphics', 'width', '800')
 Config.set('graphics', 'height', '640')
 
 
-def processImage(image, invert, WBPoint):
+def processImage(image, invert, WBPoint=None):
     # For some reason, openCV's images are flipped for Kivy
     image = cv2.flip(image, 0)
+
+    if WBPoint is not None:
+        # Split image into channels (array notation is more efficient than cv2.split
+        b = image[:, :, 0]
+        g = image[:, :, 1]
+        r = image[:, :, 2]
+
+        # Find the overall luminance of the image to correct to when white balancing
+        lum = (b + g + r) / 3
+
+        # perform the white balance, and overwrite the channels
+        b = b * lum / WBPoint[0]
+        g = g * lum / WBPoint[1]
+        r = r * lum / WBPoint[2]
+
+        image[:, :, 0] = b
+        image[:, :, 1] = g
+        image[:, :, 2] = r
 
     # If invert button is pressed, flip that puppy
     if invert:
         image = cv2.bitwise_not(image)
 
-    # Perform more processing here
-
-    #
+    # This is all code to convert it to a bitstream for Kivy
     buffer = image.tostring()
     texture = Texture.create(size=(image.shape[1], image.shape[0]), colorfmt='bgr')
     texture.blit_buffer(buffer, colorfmt='bgr', bufferfmt='ubyte')
@@ -43,33 +59,11 @@ class MainScreen(Screen):
         self.framerate = 32
         self._clock = None
         self._invert = False
+
+        self._wb = None
+        self._whitePoint = (255, 255, 255)
+
         self.start()
-
-    def start(self):
-        self._clock = Clock.schedule_interval(self.animate, 1.0 / self.framerate)
-
-    def stop(self):
-        Clock.unschedule(self._clock)
-
-    def invert(self, value):
-        self._invert = value
-
-    def animate(self, dt):
-        image = np.zeros((640, 800, 3), np.uint8)
-        image = image + 25
-
-        # Set the texture and update the fps counter
-        image, texture = processImage(image=image, invert=self._invert, WBPoint=(0,0,0))
-        self.ids.background.texture = texture
-        self.ids.fps.text = str(round(1 / dt, 1))
-
-
-class MenuScreen(Screen):
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-        self._clock = None
-        self.framerate = 32
-        self._invert = False
 
     def start(self):
         self._clock = Clock.schedule_interval(self.animate, 1.0 / self.framerate)
@@ -80,13 +74,92 @@ class MenuScreen(Screen):
     def invert(self):
         self._invert = not self._invert
 
+    def activateWB(self):
+        if np.all(self._wb == self._whitePoint):
+            self._wb = None
+        else:
+            self._wb = self._whitePoint
+
+    def setWhitePoint(self, value):
+        self._whitePoint = value
+
+    def animate(self, dt):
+        image = np.zeros((640, 800, 3), np.uint8)
+        image = image + 25
+
+        # Set the texture and update the fps counter
+        image, texture = processImage(image=image, invert=self._invert, WBPoint=self._wb)
+        self.ids.background.texture = texture
+        self.ids.fps.text = str(round(1 / dt, 1))
+
+
+class MenuScreen(Screen):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self._clock = None
+        self._framerate = 32
+        self._invert = False
+
+        self._wb = None
+        self._whitePoint = (255, 255, 255)
+        self._wbCapture = False
+        self._wbPoint = (0, 0)
+
+    def start(self):
+        self._clock = Clock.schedule_interval(self.animate, 1.0 / self._framerate)
+
+    def stop(self):
+        Clock.unschedule(self._clock)
+
+    def invert(self):
+        self._invert = not self._invert
+
+    def activateWB(self):
+        if np.all(self._wb == self._whitePoint):
+            self._wb = None
+        else:
+            self._wb = self._whitePoint
+
+    def setWhitePoint(self, value):
+        self._whitePoint = value
+
+    def getWhitePoint(self):
+        return self._whitePoint
+
+    def on_touch_up(self, touch):
+        # Touch.pos[0] is x, touch.pos[1] is y
+        # On touch if within the image, gather the 5x5 grid of pixels and get average bgr for wb
+        if (touch.pos[0] < 700.0) & (touch.pos[1] > 110.0):
+            xRatio = 800.0 / 700.0
+            yRatio = 640.0 / 540.0
+            self._wbPoint = (round(touch.pos[0] * xRatio), round((touch.pos[1] - 110) * yRatio))
+            self._wbCapture = True
+            print(self._wbPoint)
+
     def animate(self, dt):
         # Make a dummy image for me
         image = np.zeros((640, 800, 3), np.uint8)
-        image = image + 32
+        image[:, :, 0] = image[:, :, 0] + 25
+        image[:, :, 1] = image[:, :, 1] + 50
+        image[:, :, 2] = image[:, :, 2] + 75
 
-        # Process the image according to user inputs and
-        image, texture = processImage(image=image, invert=self._invert, WBPoint=(0, 0, 0))
+        # If capturing wb point get it here
+        if self._wbCapture:
+            # Cut out a 10x10 pixel area to sample white balance from
+            sample = image[self._wbPoint[1] - 5:self._wbPoint[1] + 5,
+                     self._wbPoint[0] - 5:self._wbPoint[0] + 5, :]
+
+            # average over it and set it as the white point in both the menu and main screen
+            self._whitePoint = np.mean(sample, axis=(0, 1))
+            self.manager.get_screen('main').setWhitePoint(self._whitePoint)
+
+            # Tell it to capturing white point
+            self._wbCapture = False
+
+        image = cv2.resize(image, (720, 576))
+
+        # Process the image according to user inputs
+        image, texture = processImage(image=image, invert=self._invert, WBPoint=self._wb)
         self.ids.background.texture = texture
         self.ids.hist.texture = self.genHist(image, 720, 576)
 
