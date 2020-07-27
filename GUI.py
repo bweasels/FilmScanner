a160f30b7cefce4425b86d58b7e8462eb702e2e1
Ben Wesley <benkumwes@gmail.com>
2020-07-27 10:07:58 -0400

    moving things around

--- GUI.py
+++ GUI.py
@@ -20,31 +20,10 @@ import time
 
 # Class imports
 from dummyCam import DummyVid
-from ImageProcessing import ImageProcessor
+from baseScreen import BaseScreen
 
 Config.set('graphics', 'width', '800')
 Config.set('graphics', 'height', '640')
-
-class BaseScreen(Screen):
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-        self.framerate = 32
-        self._clock = None
-
-    def start(self):
-        self._clock = Clock.schedule_interval(self.animate, 1.0 / self.framerate)
-
-    def stop(self):
-        Clock.unschedule(self._clock)
-
-    def animate(self, dt):
-        # Get the processed image and save texture
-        image, texture = App.get_running_app().stream.processImage()
-        self.ids.background.texture = texture
-
-        # Write out fps for our information
-        self.ids.fps.text = str(round(1 / dt, 1))
-
 
 class MainScreen(BaseScreen):
     def __init__(self, **kwargs):
