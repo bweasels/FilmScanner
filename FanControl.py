9769af65b0561b22f9bd35e28fd2184f105a9f70
bweasels <benkumwes@gmail.com>
2020-07-27 16:16:12 -0400

    changed fan control to reference board pins instead of internal ones

--- FanControl.py
+++ FanControl.py
@@ -2,49 +2,48 @@ import RPi.GPIO as GPIO
 import time
 
 
-FAN_PIN = 18
+FAN_PIN = 12
 WAIT_TIME = 1
 PWM_FREQ = 25000
 print("hello")
 
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(FAN_PIN, GPIO.OUT, initial=GPIO.LOW)
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(FAN_PIN, GPIO.OUT, initial=40)
 fan = GPIO.PWM(FAN_PIN, PWM_FREQ)
 
-print('setupgpIO')
+print('setup GPIO')
 
 print("pwm range set")
-
 fan.start(40)
 
 print("Min RPM")
 time.sleep(5)
 
-fan.start(70)
+fan.start(50)
 
 print("Mid 1 RPM")
 time.sleep(5)
 
-fan.start(80)
+fan.start(60)
 print("Mid 2 RPM")
 time.sleep(5)
 
-fan.start(90)
+fan.start(70)
 
 print("Mid 3 RPM")
 time.sleep(5)
 
-fan.start(100)   # mid RPM
+fan.start(80)   # mid RPM
 
 print("Mid 4 RPM")
 time.sleep(5)
 
-fan.start(110)
+fan.start(90)
 
 print("High RPM")
 time.sleep(5)
 
-fan.start(128)
+fan.start(100)
 print("Max RPM")
 time.sleep(5)
 
