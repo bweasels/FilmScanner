81cf5c82891ec47b323a33cdd0953f5ec12e5986
Ben Wesley <benkumwes@gmail.com>
2020-07-29 23:17:53 -0400

    fixig commandline bugs

--- raspiCam.py
+++ raspiCam.py
@@ -168,9 +168,9 @@ class RaspiCam:
 
 #    def capture(self, fname):
     def capture(self, shutterSpeed, exposureComp, filename):
-        cmd = 'raspistill -ISO 100 -v -fps 2 -ss ' + str(shutterSpeed) + \
+        cmd = 'raspistill -ISO 100 -v -ss ' + str(shutterSpeed) + \
               ' -ev ' + str(exposureComp) + ' -o /home/pi/Documents/FilmScanner/image.jpg' + \
-              '-t 1000'
+              ' -t 1000'
         print(cmd)
         subprocess.call(cmd, shell=True)
 
