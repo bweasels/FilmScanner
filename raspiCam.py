b9acf42f68941771b1b3e20649366afbe8fcc242
Ben Wesley <benkumwes@gmail.com>
2020-07-29 23:26:05 -0400

    trying settings

--- raspiCam.py
+++ raspiCam.py
@@ -168,7 +168,7 @@ class RaspiCam:
 
 #    def capture(self, fname):
     def capture(self, shutterSpeed, exposureComp, filename):
-        cmd = 'raspistill -ex sports -ISO 100 -v -awb off -t 100 -ss ' + str(shutterSpeed) + \
+        cmd = 'raspistill -ex sports -ISO 100 -v -awb off -t 1000 -ss ' + str(shutterSpeed) + \
               ' -ev ' + str(exposureComp) + ' -o /home/pi/Documents/FilmScanner/image.jpg'
         print(cmd)
         subprocess.call(cmd, shell=True)
