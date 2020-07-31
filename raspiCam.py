4108dad3aab433c2552b1a758e3536f5ae6e9fd7
Ben Wesley <benkumwes@gmail.com>
2020-07-31 15:48:12 -0400

    seeing if there is some gain difference between the raspistill and picamera

--- raspiCam.py
+++ raspiCam.py
@@ -170,8 +170,10 @@ class RaspiCam:
 #    def capture(self, fname):
     def capture(self, shutterSpeed, exposureComp, filename):
         cv2.waitKey(500)
-        cmd = 'raspistill -md 3 -ISO 100 -ss ' + str(shutterSpeed) + ' -set -ex off -ag 1' + \
-              ' -ev ' + str(round(exposureComp)) + ' -t 5000 -o /home/pi/Documents/FilmScanner/image.jpg'
+#        cmd = 'raspistill -md 3 -ISO 100 -ss ' + str(shutterSpeed) + ' -set -ex off -ag 1' + \
+#              ' -ev ' + str(round(exposureComp)) + ' -t 5000 -o /home/pi/Documents/FilmScanner/image.jpg'
+        cmd = 'raspistill -md 3 -ISO 100 -set -ex off -ag 1' + ' -ev ' + str(round(exposureComp)) + \
+              ' -t 5000 -o /home/pi/Documents/FilmScanner/image.jpg'
         print(cmd)
         subprocess.call(cmd, shell=True)
 
