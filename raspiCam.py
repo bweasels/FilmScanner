57d604f2c8fb29091c67e5e0969bc6116cbea3d7
Ben Wesley <benkumwes@gmail.com>
2020-07-31 15:53:22 -0400

    incrementing the settings one at a time

--- raspiCam.py
+++ raspiCam.py
@@ -174,7 +174,7 @@ class RaspiCam:
 #              ' -ev ' + str(round(exposureComp)) + ' -t 5000 -o /home/pi/Documents/FilmScanner/image.jpg'
         # cmd = 'raspistill -md 3 -ISO 100 -set -ex sun -ag 1' + ' -ev ' + str(round(exposureComp)) + \
         #       ' -t 5000 -o /home/pi/Documents/FilmScanner/image.jpg'
-        cmd = 'raspistill -md 3 -o imageTest.jpg'
+        cmd = 'raspistill -md 3 -ISO 100 -o imageTest.jpg'
         print(cmd)
         subprocess.call(cmd, shell=True)
 
