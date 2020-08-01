cc97c5adc961c639245ac9825c6b9867f5fd7362
Ben Wesley <benkumwes@gmail.com>
2020-08-01 14:21:20 -0400

    locking down awbg

--- raspiCam.py
+++ raspiCam.py
@@ -174,7 +174,7 @@ class RaspiCam:
 #              ' -ev ' + str(round(exposureComp)) + ' -t 5000 -o /home/pi/Documents/FilmScanner/image.jpg'
         # cmd = 'raspistill -md 3 -ISO 100 -set -ex sun -ag 1' + ' -ev ' + str(round(exposureComp)) + \
         #       ' -t 5000 -o /home/pi/Documents/FilmScanner/image.jpg'
-        cmd = 'raspistill -md 3 -ISO 100 -ex off -awb off -ag 1 -dg 1 -set -v -ss ' + str(shutterSpeed) + ' -o imageTest.jpg'
+        cmd = 'raspistill -md 3 -ISO 100 -ex off -awb off -ag 1 -dg 1 awbg 1.0,1.0 -set -v -ss ' + str(shutterSpeed) + ' -o imageTest.jpg'
         # cmd = 'raspiyuv -rgb '
         print(cmd)
         subprocess.call(cmd, shell=True)
