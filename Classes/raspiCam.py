6188c54936a6baa41fd3ff31955ad1950d9bf5c7
Ben Wesley <benkumwes@gmail.com>
2020-09-02 22:31:31 -0400

    What is going on!?!?

--- Classes/raspiCam.py
+++ Classes/raspiCam.py
@@ -155,8 +155,8 @@ class RaspiVid:
             self.locked = True
 
         self.camera.shutter_speed = round(self.camera.exposure_speed * 1.05)
-        print(round(self.camera.exposure_speed*1.05))
-        print(self.camera.exposure_speed)
+        print("Expected Shutter Speed: " + str(self.camera.shutter_speed) +
+              " Actual Shutter Speed: " + str(self.camera.exposure_speed))
 
 
     def decreaseSS(self):
